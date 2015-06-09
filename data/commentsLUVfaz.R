aac350a9c55dea50d201a56b35fcae2bd2f4f997
Hana Sevcikova <hsevcikova@psrc.org>
2015-06-09 15:24:03 -0700

    LUV reports

---
+++ data/commentsLUVfaz.R
@@ -0,0 +1,16 @@
+comments <- list(
+"705"="suggested adding 4000 HU and 2000 Jobs",
+"505"="Jobs are poorly represented. Provided 6500 jobs as pipeline.",
+"5825"= "Manufacturing-Industrial Centers and the jobs seem high",
+"6125"= "Manufacturing-Industrial Centers and the jobs seem high",
+ "5925"= "very low jobs despite being having two light rail stations",
+  "6115"="too many jobs",
+   "6114"= "too many jobs",
+   "5915"= "too many jobs",
+   "6216"= "doesnt get any jobs ,  but Childrens Hospital MPD",
+"6316"="Ballard - 2800 HUs added.",
+"6225"="2-3K jobs shy of Seattle policy goals",
+"5815"="Industrial zones - jobs too high?",
+"5826"="Industrial zones - jobs too high?"
+
+   )
